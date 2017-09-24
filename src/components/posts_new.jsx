6e78f143d0be046d6ddcc626b5b0121c111dc8e2
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-09-24 21:29:25 +0300

    Added validation rules

--- src/components/posts_new.jsx
+++ src/components/posts_new.jsx
@@ -3,23 +3,79 @@ import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
 
+    renderField(field){
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input type="text"
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        )
+    }
+
     render() {
         return (
             <form>
                  <div>
-                    <label>First Name</label>
+                    <label>Title</label>
                     <div>
-                    <Field
-                        name="title"
-                        component="input"
-                        type="text"
-                        placeholder="Title"
-                    />
+                        <Field
+                            name="title"
+                            component={this.renderField}
+                            type="text"
+                            placeholder="Title"
+                        />
+                    </div>
+                </div>
+                 <div>
+                    <label>Categories</label>
+                    <div>
+                        <Field
+                            name="categories"
+                            component={this.renderField}
+                            type="text"
+                            placeholder="Categories"
+                        />
+                    </div>
+                </div>
+                 <div>
+                    <label>Post Content</label>
+                    <div>
+                        <Field
+                            name="content"
+                            component={this.renderField}
+                            type="text"
+                            placeholder="Content goes here"
+                        />
                     </div>
                 </div>
             </form>
         )
     }
+}
+
+function validate(values) {
+    // values = { title: 'asdasd', catege....}
+    const errors = {};
+
+    if(!values.title){
+        errors.title = "Enter title!";
+    }    
+    
+
+    if(!values.categories){
+        errors.categories = "Enter categories!";
+    }    
+
+    if(!values.content){
+        errors.content = "Enter content!";
+    }    
+
+    // if errors is empty, the form is fine to submit
+    return errors;
 }
 
 export default reduxForm({
