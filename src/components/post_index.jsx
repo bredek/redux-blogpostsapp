dc0a8cf9512c82af751a4c72cc7c3eed7bbdc96e
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-09-24 19:04:53 +0300

    First Route Added

---
+++ src/components/post_index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class PostIndex extends Component{
+  render(){
+    return <div>PostIndex</div> 
+  }
+}
+
+export default PostIndex;
--- src/index.js
+++ src/index.js
@@ -4,33 +4,17 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
+import PostIndex from './components/post_index';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component{
-  render(){
-    return <div>Hello!</div> 
-  }
-}
-
-class GoodBuy extends React.Component{
-  render(){
-    return <div>Good Buy!</div> 
-  }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        Header
-        <Route path="/hello" component={Hello} />
-        <Route path="/buy" component={GoodBuy} />
-        Footer
+        <Route path="/" component={PostIndex} />
       </div> 
     </BrowserRouter>
-    {/* <App /> */}
   </Provider>
   , document.querySelector('.container'));
