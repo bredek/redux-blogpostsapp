d8e01fed69c81a98e612e204387a52c89f3a7fe6
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-09-24 22:45:24 +0300

    Selecting from own props

--- src/components/posts_show.jsx
+++ src/components/posts_show.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// actions
+import { fetchPost } from '../actions/index';
 
 class PostsShow extends Component {
+
+    componentDidMount() {
+        const {id }= this.props.match.params;
+        // this.props.match.params.id;
+        this.props.fetchPost(id);
+    }
+
+
 
     render(){
         return(
@@ -9,6 +20,14 @@ class PostsShow extends Component {
     }
 }
 
-export default PostsShow;
+// whatever is returned will show up as props
+// Object
+function mapStateToProps({ posts }, ownProps) {
+    return {
+        post: posts[ownProps.match.params.id]
+    };
+}
 
+// Action creator shrtcuts
+export default connect(null, { fetchPost })(PostsShow);
 // export default PostsNew;
