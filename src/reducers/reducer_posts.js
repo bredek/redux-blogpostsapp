dc183b6f68bec520947300dbef8c16a0a835dc29
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-09-24 19:42:27 +0300

    Posts reducer added

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import {FETCH_POSTS} from '../actions/index';
+
+export default function (state = {}, action) {
+    switch (action.typ) {
+        case FETCH_POSTS:
+            console.log(action.payload);
+            return _.mapKeys(action.payload.data, 'id');
+            break;
+        default:
+            return state;
+            break;
+    }
+}
