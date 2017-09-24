772928eb4cdab8463152f61c7dff3438129274c3
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-09-24 21:07:14 +0300

    Setting for ReduxForm
    Added a simple Field

--- src/components/posts_new.jsx
+++ src/components/posts_new.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
 
     render() {
         return (
-            <div>
-                Posts New
-            </div>
+            <form>
+                 <div>
+                    <label>First Name</label>
+                    <div>
+                    <Field
+                        name="title"
+                        component="input"
+                        type="text"
+                        placeholder="Title"
+                    />
+                    </div>
+                </div>
+            </form>
         )
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm' // a unique identifier for this form
+})(PostsNew)
+
+// export default PostsNew;
