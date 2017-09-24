7672a564f9711effaf2c41aa70ab2dd1d96bdc89
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-09-24 18:52:05 +0300

    BrowserRouter basics

--- src/index.js
+++ src/index.js
@@ -2,14 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component{
+  render(){
+    return <div>Hello!</div> 
+  }
+}
+
+class GoodBuy extends React.Component{
+  render(){
+    return <div>Good Buy!</div> 
+  }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        Header
+        <Route path="/hello" component={Hello} />
+        <Route path="/buy" component={GoodBuy} />
+        Footer
+      </div> 
+    </BrowserRouter>
+    {/* <App /> */}
   </Provider>
   , document.querySelector('.container'));
