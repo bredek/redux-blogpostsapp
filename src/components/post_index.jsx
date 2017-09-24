b534f2a0f94152f5f84bfc9100b87fe640b02fbd
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-09-24 19:54:10 +0300

    Post Index Container created

--- src/components/post_index.jsx
+++ src/components/post_index.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
+// functions
+import { connect } from 'react-redux';
+// actions
+import { fetchPosts } from '../actions/index';
 
-class PostIndex extends Component{
-  render(){
-    return <div>PostIndex</div> 
-  }
+
+class PostIndex extends Component {
+
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
+
+    render() {
+        return <div>PostIndex</div>
+    }
 }
-
-export default PostIndex;
+// Action creator shrtcuts
+export default connect(null, { fetchPosts })(PostIndex);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { ReduxPromise } from 'redux-promise';
+import ReduxPromise  from 'redux-promise';
 
 import PostIndex from './components/post_index';
 import reducers from './reducers';
